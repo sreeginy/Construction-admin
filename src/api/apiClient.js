8893961e6315e1cfbe2486e7cb89e11ecaeebba6
Sreeginy Kodeeswaran <sreeginy98@gmail.com>
2023-04-02 22:36:47 +0530

    update

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,5 +2,5 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: process.env.REACT_APP_BACKEND_BASE_URL || "https://backend-api.ammo-db.dev"
+    baseURL: process.env.REACT_APP_BACKEND_BASE_URL || "http://localhost:8082/"
 })
