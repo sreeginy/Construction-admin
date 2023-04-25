b3944eb45e62d88089a5709b35f2f063ab0d7399
Sreeginy <sreeginy98@gmail.com>
2023-04-25 16:28:52 +0530

    Update index.jsx

--- src/scenes/employee/index.jsx
+++ src/scenes/employee/index.jsx
@@ -375,29 +375,7 @@ export default function Customer() {
                 )}
               </TableBody>
 
-              {isNotFound && (
-                <TableBody>
-                  <TableRow>
-                    <TableCell align="center" colSpan={6} sx={{ py: 3 }}>
-                      <Paper
-                        sx={{
-                          textAlign: 'center',
-                        }}
-                      >
-                        <Typography variant="h6" paragraph>
-                          Not found
-                        </Typography>
 
-                        <Typography variant="body2">
-                          No results found for &nbsp;
-                          <strong>&quot;{filterName}&quot;</strong>.
-                          <br /> Try checking for typos or using complete words.
-                        </Typography>
-                      </Paper>
-                    </TableCell>
-                  </TableRow>
-                </TableBody>
-              )}
             </Table>
           </TableContainer>
 
