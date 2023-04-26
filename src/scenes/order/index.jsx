94cc7d8220a4cd0600a52fcb348e4124d412ad71
Sreeginy <sreeginy98@gmail.com>
2023-04-26 18:55:45 +0530

    Update index.jsx

--- src/scenes/order/index.jsx
+++ src/scenes/order/index.jsx
@@ -359,7 +359,6 @@ export default function Order() {
           >
             Create Order
           </Button>
-
 )}
         </Stack>
 
@@ -472,8 +471,6 @@ export default function Order() {
        
             </Table>
           </TableContainer>
-
-
           <TablePagination
             rowsPerPageOptions={[5, 10, 25]}
             component="div"
@@ -486,37 +483,6 @@ export default function Order() {
         </Card>
       </Container>
       <ToastContainer/>
-
-      {/* <Popover
-        open={Boolean(open)}
-        anchorEl={open}
-        onClose={handleCloseMenu}
-        anchorOrigin={{ vertical: 'top', horizontal: 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: 'right' }}
-        PaperProps={{
-          sx: {
-            p: 1,
-            width: 140,
-            '& .MuiMenuItem-root': {
-              px: 1,
-              typography: 'body2',
-              borderRadius: 0.75,
-            },
-          },
-        }}
-      >
-        <MenuItem
-        >
-          <Iconify icon={'eva:edit-fill'} sx={{ mr: 2 }} />
-          Edit
-        </MenuItem>
-  
-        <MenuItem sx={{ color: 'error.main' }}
-       >
-          <Iconify icon={'eva:trash-2-outline'} sx={{ mr: 2 }} />
-          Delete
-        </MenuItem>
-      </Popover> */}
     </>
   );
 }
